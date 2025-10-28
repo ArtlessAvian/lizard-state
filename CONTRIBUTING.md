e7a3da7f307bc94b062aea8cdf738b4351b1bce6
Ryan <artlessavian@gmail.com>
2025-10-27 18:20:26 -0700

    planning things, i suppose

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Branches
+
+* `main` is always clean. Everything must work as intended, and all tests must pass.
+* `dev-godot` is allowed to not have godot working. The rust tests must pass.
+* `dev-rust` is allowed to break everything.
+
+All branches must merge into a matching development branch.
--- rust-workspace/lizstate-crawler/src/lib.rs
+++ rust-workspace/lizstate-crawler/src/lib.rs
@@ -1,2 +1,41 @@
+//! A traditional roguelike, dungeon crawl engine. Tailored for Lizard State.
+//!
+//! Besides the usual features, there are plans for:
+//! * easy undo, save, and load.
+//! * delayed commands.
+//! * stun and whatnot.
+//!
+//! The *game* design for those decisions is out of scope here.
+//! TL;DR it's sort of fighting game like.
+
 #![warn(clippy::pedantic)]
 #![warn(clippy::allow_attributes_without_reason)]
+
+/// `Tile`s, positions that are gridlike, that form a funky `Map`.
+///
+/// `Map`s and their `Tile`s can be projected into a grid, but the original is not the plane.
+pub mod map;
+
+/// The 'Entity' trait describes things that should be drawn to the screen.
+pub mod entity;
+
+/// A `Creature` is a concrete type implementing `Entity`.
+///
+/// They have a few basic mutable operations.
+/// They are also partially ordered by turn order.
+pub mod creature;
+
+/// A `Floor` contains `Creature`s, a `Map`, and other resources/systems.
+///
+/// `Floor`s can be cloned easily for undo, quicksave, and quickload.
+/// `Floor`s can also be serialized and deserialized.
+///
+/// In the context of a `Floor`, `Now` is the smallest `Turn`.
+/// A `Subject` is a `Floor` and its turn taker.
+pub mod floor;
+
+/// The `Command` trait describe how a `Floor` changes in a turn and produces a `Log`.
+pub mod commands;
+
+/// Reexports from the other modules. The usual stuff.
+pub mod prelude;
