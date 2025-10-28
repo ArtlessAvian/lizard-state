4d14e09e5971ed173cbca1e191beeab4786822b1
Ryan <artlessavian@gmail.com>
2025-10-27 19:11:14 -0700

    things in a space

--- rust-workspace/lizstate-crawler/src/creature.rs
+++ rust-workspace/lizstate-crawler/src/creature.rs
@@ -1,1 +1,24 @@
+use crate::entity::Entity;
+use crate::map::GridLike;
+use crate::map::GridPos;
 
+#[non_exhaustive]
+pub struct Creature<Pos: GridLike = GridPos> {
+    pos: Pos,
+}
+
+impl<Pos: GridLike> Creature<Pos> {
+    pub fn new(pos: Pos) -> Self {
+        Self { pos }
+    }
+}
+
+impl<Pos: GridLike> Entity for &Creature<Pos> {
+    fn get_char(&self) -> char {
+        '@'
+    }
+
+    fn get_flat_position(&self) -> (i32, i32) {
+        self.pos.flatten()
+    }
+}
--- rust-workspace/lizstate-crawler/src/entity.rs
+++ rust-workspace/lizstate-crawler/src/entity.rs
@@ -1,1 +1,7 @@
+pub trait Entity {
+    /// This is not useful for graphical clients, but can be used for fun.
+    fn get_char(&self) -> char;
 
+    /// This is naive and ignores layers and vision.
+    fn get_flat_position(&self) -> (i32, i32);
+}
