12a00cff1e66d6e4e0bf1792dbfd7acb3d05052f
Ryan <artlessavian@gmail.com>
2025-11-10 18:32:00 -0800

    cleanup

--- rust-workspace/lizstate-sequence/src/math.rs
+++ rust-workspace/lizstate-sequence/src/math.rs
@@ -3,22 +3,3 @@
 pub mod commutative_ring;
 
 pub mod polynomial;
-
-///////////////////////////
-
-// mod ring;
-
-// pub(crate) mod power_series;
-
-// /// Generating functions, expressed in closed form.
-// ///
-// /// We can evaluate them at specific values for fun.
-// pub(crate) mod generating;
-
-// No, we won't need sequences of arbitrarily large integers.
-// I mean, it would be cool though.
-// mod continued_fractions.
-
-// #[cfg(test)]
-// /// If you need an explanation, it'll be here.
-// mod what_the_helly;
