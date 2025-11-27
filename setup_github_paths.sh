d994b261b9df3701159f19c539639120518cdb1d
Ryan <artlessavian@gmail.com>
2025-11-27 02:30:49 -0800

    aight this should work

---
+++ setup_github_paths.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ "$HOME" == "/root" ]] 
+then
+    echo "Current user's ($USER) home folder is /root... nothing to do."
+    exit 0
+fi
+
+mkdir -pv "$HOME"
+ln -sv /root/* "$HOME"
+ln -sv /root/.?[[:alpha:]]* "$HOME"
+chown -R "$USER" "$HOME"
